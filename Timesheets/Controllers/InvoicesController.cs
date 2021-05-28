7c5c840499ad91c9091ff25f4b297825cd03931b
evgshk <evgshk@gmail.com>
2021-05-28 19:18:34 +0300

    Добавить api создания счета

--- Timesheets/Controllers/InvoicesController.cs
+++ Timesheets/Controllers/InvoicesController.cs
@@ -10,16 +10,25 @@ namespace Timesheets.Controllers
     public class InvoicesController: TimesheetBaseController
     {
         private readonly IInvoiceManager _invoiceManager;
+        private readonly IContractManager _contractManager;
         
-        public InvoicesController(IInvoiceManager invoiceManager)
+        public InvoicesController(IInvoiceManager invoiceManager, IContractManager contractManager)
         {
             _invoiceManager = invoiceManager;
+            _contractManager = contractManager;
         }
         
         /// <summary> Создает клиентский счет </summary>
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] InvoiceRequest invoiceRequest)
         {
+            var isAllowedToCreate = await _contractManager.CheckContractIsActive(invoiceRequest.ContractId);
+
+            if (isAllowedToCreate !=null && !(bool)isAllowedToCreate)
+            {
+                return BadRequest($"Contract {invoiceRequest.ContractId} is not active or not found.");
+            }
+            
             var id = await _invoiceManager.Create(invoiceRequest);
             return Ok(id);
         }
